a61e1a1b22ff0b70ecb0dffe61855a333639a2f5
proximor <vitdelion@gmail.com>
2022-11-08 22:03:25 +0600

    Bugs: almost all bugs fixed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,30 +1,34 @@
 function getResult(a, b, c) {
     'use strict';
-    const discrim = b ** 2 - 4 * a * (-c);
+    const discrim = b ** 2 - 4 * a * c;
     const x1 = (-b + discrim ** (1 / 2)) / (2 * a);
     const x2 = (-b - discrim ** (1 / 2)) / (2 * a);
 
-    if (discrim < 0) {
-        return [];
+    if (discrim > 0) {
+        return [x1, x2];
     } else if (discrim === 0) {
-        return [x1, x2];
-    } else if (discrim > 0) {
-        return [x1, x2];
-    }
-}
-
-function getAverageMark(marks) {
-    if (marks.length > 5) {
-        return marks.slice(0, 5).reduce((sum, curr) => sum + curr, 0);
+        return [x1];
     } else {
         return [];
     }
 }
 
+function getAverageMark(marks) {
+    if (marks.length === 0) {
+        return 0;
+    } else if (marks.length >= 5) {
+        return +((marks.slice(0, 5).reduce((sum, curr) => sum + curr, 0)) / 5).toFixed();
+    } else if (marks.length < 5 && marks.length > 0) {
+        return +((marks.reduce((sum, curr) => sum + curr, 0)) / marks.length).toFixed();
+    }
+}
+
 function askDrink(name, dateOfBirthday) {
     const year = new Date().getFullYear();
-    const userAge = year - dateOfBirthday;
-    let drinkAccess = (userAge > 18)
-        ? console.log(`“Не желаете ли олд-фэшн, ${name}?”`)
-        : console.log(`“Сожалею, ${name}?, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!”`);
+    let userAge = year - dateOfBirthday;
+    if (userAge > 18) {
+        return `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
+    }
 }
