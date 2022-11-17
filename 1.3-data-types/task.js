dfec7e186b60bb6846bf04394e76ac320467be97
proximor <vitdelion@gmail.com>
2022-11-17 11:19:16 +0600

    Tasks: 2 tasks done

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,17 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    'use strict';
+    const p = (+percent) / 100;
+    const c = +contribution;
+    const a = +amount;
+    const creditAmount = a - c;
+    const currentMonth = new Date().getMonth();
+    const nom = currentMonth - date.getMonth();
+    const totalAmount = creditAmount * (p + p / (((1 + p) ** nom) - 1)).toFixed(2);
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name === '') return 'Привет, мир! Меня зовут Аноним.';
+    return `Привет, мир! Меня зовут ${name}`;
 }
+
