8f087ed75c7f1028bfe4153ebf63093ac4366766
proximor <vitdelion@gmail.com>
2022-11-21 21:41:17 +0600

    Code optimized: some code optimized

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,20 +1,24 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     'use strict';
-    const p = (+percent) / 100;
-    const c = +contribution;
-    const a = +amount;
+    const p = (percent) / 100;
+    const c = contribution;
+    const a = amount;
+
+    if (p !== Number.isNaN(parseInt((p))) return `Параметр ${p} содержит неправильное значение ${percent}`;
+    if (c !== Number.isNaN(parseInt(c))) return `Параметр ${c} содержит неправильное значение ${contribution}`;
+    if (a !== Number.isNaN(parseInt(a))) return `Параметр ${a} содержит неправильное значение ${amount}`;
+
     const creditAmount = a - c;
     const nextYearDate = new Date(new Date().setFullYear(new Date().getFullYear() + 3));
-    const period = nextYearDate - new Date().getFullYear;
-    const monthNum = period * 12;
-    const totalAmount = creditAmount * (p + p / (((1 + p) ** monthNum) - 1)).toFixed(2);
+    const creditPeriod = nextYearDate.getFullYear() - new Date().getFullYear();
+    const monthNum = creditPeriod * 12;
+    const totalAmount = creditAmount * (p + p / (((1 + p) ^ monthNum) - 1)).toFixed(2);
     return totalAmount;
 }
 
+console.log(calculateTotalMortgage(10, 0, 50000, 2025));
+
 function getGreeting(name) {
-    const validUsers = ['Пётр'];
-    let anonymous = 'Аноним';
-    let result = (!validUsers.includes(name))? `Привет, мир! Меня зовут ${anonymous}.` : `Привет, мир! Меня зовут ${name}.`;
-    return result;
+    return (!name) ? `Привет, мир! Меня зовут Аноним.` : `Привет, мир! Меня зовут ${name}.`;
 }
 
