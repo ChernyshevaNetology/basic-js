96bb8e60d48a4263f9003d9249f7d98c4adf4661
proximor <vitdelion@gmail.com>
2022-11-23 11:42:20 +0600

    Bug fixing: almost all bugs fixed but two

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -3,27 +3,30 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     const p = (parseInt(percent)) / 100;
     const c = parseInt(contribution);
     const a = parseInt(amount);
-    const d = date;
 
     if (Number.isNaN(p)) {
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    } else if (p < 0) {
         return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
     }
     if (Number.isNaN(c)) {
         return `Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}`;
+    } else if (c < 0) {
+        return `Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}`;
     }
     if (Number.isNaN(a)) {
         return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+    } else if (a < 0) {
+        return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
     }
-    if (d.getFullYear < new Date().getFullYear()) {
+    if (new Date(date).getFullYear() < new Date().getFullYear()) {
         return `Параметр "Срок ипотеки" содержит неправильное значение ${date}`;
     }
-
-    const creditPeriod = date.getFullYear() - new Date().getFullYear();
+    const creditPeriod = (new Date(date).getFullYear() - new Date().getFullYear()) * 12;
     const creditAmount = a - c;
     const monthlyInterest = p / 12;
-    const monthNum = creditPeriod * 12;
-    const monthlyPayment = creditAmount * (monthlyInterest + monthlyInterest / ((1 + monthlyInterest) ** monthNum - 1));
-    return  +(monthlyPayment * creditPeriod).toFixed(2);
+    const monthlyPayment = creditAmount * (monthlyInterest + monthlyInterest / ((1 + monthlyInterest) ** creditPeriod - 1));
+    return +(monthlyPayment * creditPeriod).toFixed(2);
 }
 
 function getGreeting(name) {
