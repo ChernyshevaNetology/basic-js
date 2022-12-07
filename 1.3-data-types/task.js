a3d9614f6e8e0ace633716dba3ffe4cd6ce40aba
proximor <vitdelion@gmail.com>
2022-12-07 21:27:58 +0600

    First level optimization applied

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,36 +1,45 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     'use strict';
     const p = (parseInt(percent)) / 100;
-    const c = parseInt(contribution);
-    const a = parseInt(amount);
+    const values = [
+        {
+            paramName: 'Процентная ставка',
+            val: 'percent'
+        },
+        {
+            paramName: 'Первоначальный взнос',
+            val: 'contribution'
+        },
+        {
+            paramName: 'Общая стоимость',
+            val: 'amount'
+        },
+        {
+            paramName: 'Срок ипотеки',
+            val: 'date'
+        }
+    ];
 
-    if (Number.isNaN(p)) {
-        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
-    } else if (p < 0) {
-        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    if (val instanceof Date) {
+        if (new Date(date).getFullYear() < new Date().getFullYear()) {
+            return `Параметр ${paramName} содержит неправильное значение ${date.toLocaleString()}`;
+        }
+        if (isNaN(val) || val < 0) {
+            `Параметр ${paramName} содержит неправильное значение ${val}`;
+        }
     }
-    if (Number.isNaN(c)) {
-        return `Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}`;
-    } else if (c < 0) {
-        return `Параметр "Первоначальный взнос" содержит неправильное значение ${contribution}`;
-    }
-    if (Number.isNaN(a)) {
-        return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
-    } else if (a <= 0) {
-        return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
-    }
-    if (new Date(date).getFullYear() < new Date().getFullYear()) {
-        return `Параметр "Срок ипотеки" содержит неправильное значение ${date.toLocaleString()}`;
-    }
+
     const creditPeriod = (new Date(date).getFullYear() - new Date().getFullYear()) * 12;
-    if (c >= a) return 0;
-    const creditAmount = a - c;
+
+    if (contribution >= amount) return 0;
+
+    const creditAmount = amount - contribution;
     const monthlyInterest = p / 12;
     const monthlyPayment = creditAmount * (monthlyInterest + monthlyInterest / ((1 + monthlyInterest) ** creditPeriod - 1));
     return +(monthlyPayment * creditPeriod).toFixed(2);
 }
 
 function getGreeting(name) {
-    return (!name) ? `Привет, мир! Меня зовут Аноним.` : `Привет, мир! Меня зовут ${name}.`;
+    return `Привет, мир! Меня зовут ${name ? name : 'Аноним'}.`;
 }
 
