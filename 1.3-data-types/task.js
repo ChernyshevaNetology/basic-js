235c472162cdc6a5c1f2ab608a471f82a2ce7678
proximor <vitdelion@gmail.com>
2022-12-08 22:03:07 +0600

    Final strokes: all optimization measures taken

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,21 +1,20 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     'use strict';
-    const p = (parseInt(percent)) / 100;
     const values = [
         {
-            paramName: 'Процентная ставка',
-            val: p
+            paramName: "Процентная ставка",
+            val: percent
         },
         {
-            paramName: 'Первоначальный взнос',
+            paramName: "Первоначальный взнос",
             val: contribution
         },
         {
-            paramName: 'Общая стоимость',
+            paramName: "Общая стоимость",
             val: amount
         },
         {
-            paramName: 'Срок ипотеки',
+            paramName: "Срок ипотеки",
             val: date
         }
     ];
@@ -23,20 +22,17 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
         for (const val of values) {
         if (val.val instanceof Date) {
             if (new Date(date).getFullYear() < new Date().getFullYear()) {
-                return `Параметр ${val.paramName} содержит неправильное значение ${date.toLocaleString()}`;
+                return `Параметр "${val.paramName}" содержит неправильное значение ${date.toLocaleString()}`;
             }
         }
         if (isNaN(val.val) || val.val < 0) {
-            `Параметр ${val.paramName} содержит неправильное значение ${val}`;
+            return `Параметр "${val.paramName}" содержит неправильное значение ${val.val}`;
         }
     }
-
-    const creditPeriod = (new Date(date).getFullYear() - new Date().getFullYear()) * 12;
-
     if (contribution >= amount) return 0;
-
+    const creditPeriod = (new Date(date).getFullYear() - new Date().getFullYear()) * 12;
     const creditAmount = amount - contribution;
-    const monthlyInterest = p / 12;
+    const monthlyInterest = (percent / 100) / 12;
     const monthlyPayment = creditAmount * (monthlyInterest + monthlyInterest / ((1 + monthlyInterest) ** creditPeriod - 1));
     return +(monthlyPayment * creditPeriod).toFixed(2);
 }
