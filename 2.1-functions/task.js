a2a5c50d1511d621f2783ca0057ef3e41fe63a56
Vito <example@gmail.com>
2023-04-12 19:49:22 +0600

    Tasks: two tasks solved

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,8 +1,65 @@
-function getAverageScore(data) {
-
-}
-
-
 function getAverageMark(marks) {
-
+    if (marks.length === 0) return 0;
+    return marks.reduce((acc, curr) => acc + curr, 0) / marks.length;
 }
+
+function getAverageScore(data) {
+    if(Object.keys(data).length === 0) {
+        return {average: 0};
+    }
+
+    const averageSubj = Object.entries(data).reduce((acc, [subj, val]) => {
+        acc[subj] = getAverageMark(val);
+        return acc;
+    }, {});
+
+    const average = Object.values(averageSubj).reduce((acc, val) => acc + val, 0) / Object.keys(averageSubj).length;
+    averageSubj.average = average;
+    return averageSubj;
+}
+
+
+function getPersonData(secretData) {
+    return Object.entries(secretData).reduce((acc, [key, val]) => {
+        acc[key] = getDecodedValue(val);
+        return acc;
+    }, {});
+}
+
+
+function getPersonData(secretData) {
+    let targetObject = Object.entries(secretData).reduce((acc, [key, val]) => {
+        acc[key] = getDecodedValue(val);
+        return acc;
+    }, {});
+    targetObject['firstName'] = targetObject['aaa'];
+    targetObject['lastName'] = targetObject['bbb'];
+    delete targetObject['aaa'];
+    delete targetObject['bbb'];
+    return targetObject;
+}
+function getPersonData(secretData) {
+    let targetObject = Object.entries(secretData).reduce((acc, [key, val]) => {
+        acc[key] = getDecodedValue(val);
+        return acc;
+    }, {});
+    targetObject['firstName'] = targetObject['aaa'];
+    targetObject['lastName'] = targetObject['bbb'];
+    delete targetObject['aaa'];
+    delete targetObject['bbb'];
+    return targetObject;
+}
+
+function getDecodedValue(secret) {
+    let val = '';
+    if (secret === 0) {
+        val = 'Родриго';
+    } else {
+        val = 'Эмильо';
+    }
+    return val;
+}
+
+
+
+
